e30ffb9e9c74402a1cdf7a57c3420f6b14dfe9e4
BL4Z3K1NG <elyoan2001@gmail.com>
2025-07-19 07:55:19 -0400

    Add files via upload

---
+++ app.py
@@ -0,0 +1,19 @@
+from flask import Flask
+from flask_socketio import SocketIO, emit
+
+
+app = Flask(__name__)
+socketio = SocketIO(app)
+
+@socketio.on('message')
+def handleMessage(msg):
+    print(f'Message: {msg}')
+    emit('message', msg, broadcast=True)
+
+@app.route('/')
+def index():
+    return "Working"
+
+
+if __name__ == '__main__':
+    socketio.run(app, debug=True)
